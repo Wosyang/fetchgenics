74998f98bef34a623f66889a4ce954d6a760b072
Wosyang <goodprayogi@gmail.com>
2023-06-17 11:20:49 +0700

    fetchApi

---
+++ fetchApi.js
@@ -0,0 +1,8 @@
+const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+
+    console.log(data);
+};
+
+fetchData();
